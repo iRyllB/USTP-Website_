f0e3d6cce2202bc26c586c7d2b45e034eabf30e8
Kent Jasper Cabunoc Sisi <2081367@g.cu.edu.ph>
2025-01-13 15:52:16 +0800

    Reintroduce About Us page and update navigation
    - Added About Us route back to App.js and NavigationBar.
    - Updated About Us page with new content sections and styling.
    - Adjusted height for various pages (aboutUs, news, events) for better layout.
    - Cleaned up CSS by removing unused styles from events.css and home.css.
    - Included main.css in events and home pages for consistent styling.

--- src/pages/aboutUs.js
+++ src/pages/aboutUs.js
@@ -1,6 +1,7 @@
 import NavigationBar from "../components/navBar";
 import Footer from "../components/footer";
 import './aboutUs.css'
+import { Link } from "react-router-dom";
 
 export default function AboutUs() {
     
@@ -16,6 +17,31 @@ export default function AboutUs() {
 
 
             </section>
+            <section className="section-1">
+                    <div className="section-1-container">
+                        <div className="about-content">
+                            <div className="about-left">
+                            </div>
+                            <div className="about-right">
+                                <h2>Lorem ipsum dolor sit amet</h2>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan.</p>
+                                <div style={{ width: 494 }}>
+                                    <Link to="/aboutus">
+                                        <button>Read More</button>
+                                    </Link>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="team">
+                            <div className="team-left">
+                                <h2>Lorem ipsum dolor sit amet</h2>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan.</p>
+                            </div>
+                            <div className="team-right">
+                            </div>
+                        </div>
+                    </div>
+                </section>
 
 
 
--- src/pages/news.js
+++ src/pages/news.js
@@ -1,6 +1,10 @@
 import NavigationBar from "../components/navBar";
 import Footer from "../components/footer";
-import './news.css'; 
+import './main.css'
+
+import './news.css';
+import { Link } from "react-router-dom";
+
 export default function News() {
     return (
         <>
@@ -11,6 +15,32 @@ export default function News() {
                 <h1>News</h1>
             </section>
 
+
+            <section className="section-1">
+                    <div className="section-1-container">
+                        <div className="about-content">
+                            <div className="about-left">
+                            </div>
+                            <div className="about-right">
+                                <h2>Lorem ipsum dolor sit amet</h2>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan.</p>
+                                <div style={{ width: 494 }}>
+                                    <Link to="/aboutus">
+                                        <button>Read More</button>
+                                    </Link>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="team">
+                            <div className="team-left">
+                                <h2>Lorem ipsum dolor sit amet</h2>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan.</p>
+                            </div>
+                            <div className="team-right">
+                            </div>
+                        </div>
+                    </div>
+                </section>
             <Footer />
         </>
     );
